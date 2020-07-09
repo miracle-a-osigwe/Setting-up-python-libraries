cc845e074c74658076d4491e85182b7e05986bd8
miracle-a-osigwe <67804322+miracle-a-osigwe@users.noreply.github.com>
2020-07-09 08:15:31 +0100

    Update Py_requirements.py

--- Python Libraries/Py_requirements.py
+++ Python Libraries/Py_requirements.py
@@ -1,7 +1,6 @@
 """
     This piece of code is to install the necessary libraries into your system.
 """
+#Let's verify that you have python installed on your system
 import sys
 import os
-
-os.environ(set=)
